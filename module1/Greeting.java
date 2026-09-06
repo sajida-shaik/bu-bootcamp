31fa565101d2652a75246d0564e3fda582f278de
Sajida Shaik <sksaji@gmail.com>
2026-09-06 10:02:49 -0400

    Add Module 1 activity: Hello World and Greeting programs

---
+++ module1/Greeting.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+public class Greeting { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("What is your name? "); 
+        String name = scanner.nextLine(); 
+        System.out.println("Hello, " + name + "! As a technical lead, you are in exactly the right place."); 
+        scanner.close(); 
+    } 
+} 
